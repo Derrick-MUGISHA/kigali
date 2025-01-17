5c228387fdbfe3162450a428e2c8a499904b0c42
Derrick-MUGISHA <derrickmugisha169@gmail.com>
2025-01-17 23:35:48 +0200

    new imprementation

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,9 +1,12 @@
 function ListGroup() {
-  const items = ["Item 1", "Item 2", "Item 3", "Item 4"];
+  let items = ["Item 1", "Item 2", "Item 3", "Item 4"];
+
+  items = [];
 
   return (
     <>
       <h1>ListGroup</h1>
+      {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
